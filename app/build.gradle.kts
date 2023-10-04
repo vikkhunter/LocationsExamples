f8ae53906ad1ac8d57b63ac7d5127bbaa45af8ff
vikhunter <codeheister91@gmail.com>
2023-10-04 13:34:58 +0530

    Added location on map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    implementation("com.google.android.gms:play-services-maps:18.1.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- app/src/main/java/com/example/locationsexamples/MainActivity.kt
+++ app/src/main/java/com/example/locationsexamples/MainActivity.kt
@@ -1,11 +1,88 @@
 package com.example.locationsexamples
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    lateinit var googleMap: GoogleMap
+    lateinit var currentLocationLatLng: LatLng
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestLocationPermissions()
+        val mapFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    private fun requestLocationPermissions() {
+        val listOfPermissions = arrayListOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        val isPermissionGranted = listOfPermissions.filter {
+            ActivityCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }
+
+        if (isPermissionGranted.isNotEmpty()) {
+            ActivityCompat.requestPermissions(
+                this, listOfPermissions.toTypedArray(), PERMISSION_REQUEST_CODE
+            )
+        } else {
+            fetchLocations()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                fetchLocations()
+            }
+        }
+
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun fetchLocations() {
+        val fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationProviderClient.lastLocation.addOnSuccessListener { location ->
+            currentLocationLatLng = LatLng(location.latitude, location.longitude)
+            if (::googleMap.isInitialized) {
+                googleMap.addMarker(
+                    MarkerOptions().position(currentLocationLatLng).title("current Location")
+                )
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocationLatLng, 15f))
+            }
+        }.addOnFailureListener {
+            Log.d("Current Location", "fetchLocations: ${it.message}")
+        }
+    }
+
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 100
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
     }
 }
